0302f834539ce0f31342f579603bcd3afa75db05
catuhub <fr.caturano@studenti.unina.it>
2018-12-12 11:57:56 +0100

    add solution to Mirai Lab

--- .docker-images/update.sh
+++ .docker-images/update.sh
@@ -12,11 +12,11 @@ pull_image() {
 }
 # Built images
 pull_image nsunina/mirai_cnc:v1.0
-pull_image nsunina/quagga:v1.0
+pull_image nsunina/quagga:v1.1
 pull_image nsunina/exabgp:3.4.20
 pull_image nsunina/mirai_bot:v1.1
 pull_image nsunina/fnm_mongodb:v1.0
-pull_image nsunina/fastnetmon:v1.3
+pull_image nsunina/fastnetmon:v1.4
 pull_image nsunina/alpine_netcat:v1.1
 pull_image nsunina/smtp-user-enum:v1.0
 pull_image nsunina/alpine_vrfy:v1.0
