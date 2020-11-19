d23862608edb725c7c0b8a074f760fd37709d6b9
peppe <undefined>
2020-11-19 15:33:44 +0100

    upgrader images

--- ssl-sniffing-mitm-lab/sniffer/start.sh
+++ ssl-sniffing-mitm-lab/sniffer/start.sh
@@ -1,6 +1,5 @@
-#!/bin/bash
 set -m
-route add default gw 192.168.2.2
 route add default gw 192.168.3.2
 route add default gw router
-/bin/bash 
+/usr/bin/xpra start --daemon=no --no-printing
+/bin/bash
